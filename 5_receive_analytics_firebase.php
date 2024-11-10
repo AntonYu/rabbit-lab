eb3d3138492a5ed2bc0d46f7cb9b6f2afd6f532f
Anton Yurchenko <webbymasta@gmail.com>
2024-11-10 13:10:50 +0400

    topic for all messages

---
+++ 5_receive_analytics_firebase.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->exchange_declare('analytics_topic', 'topic', false, true, false);
+
+$queue_name = "firebase_queue";
+$channel->queue_declare($queue_name, false, false, true, false);
+$channel->queue_bind($queue_name, 'analytics_topic', '#');
+
+echo " [*] Waiting for message at $queue_name. To exit press CTRL+C\n";
+
+$callback = function ($msg) {
+    echo ' [x] ', $msg->getRoutingKey(), ':', $msg->getBody(), "\n";
+};
+
+$channel->basic_consume($queue_name, '', false, true, false, false, $callback);
+
+try {
+    $channel->consume();
+} catch (\Throwable $exception) {
+    echo $exception->getMessage();
+}
+
+$channel->close();
+$connection->close();
